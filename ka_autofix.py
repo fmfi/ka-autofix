b7c5dc2e8cde6827d6e44a514825b9a9e51b4b0a
Martin Sucha <martin.sucha@fmph.uniba.sk>
2014-06-14 02:20:49 +0200

    Pridana kontrola SP adresara

--- ka_autofix.py
+++ ka_autofix.py
@@ -1,7 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import re
 from rtf import ByteStream, flatten, parse, tokenize, walk_left, find_text, filter_control_word, node_range, walk_right, \
     as_text, dfs_ltr, document_content, match_control_word, split_by, split_end_by
+from enum import Enum
+
+RE_TITULY = r'(?:Bc|Mgr|PhD|Ing)'
+RE_SP = r'SP_\d+(?:[.]\d+)*_' + RE_TITULY + r'(?:_' + RE_TITULY + r')*_[a-zA-Z0-9_-]+'
+
+PAT_SP_DIR = re.compile('^{}$'.format(RE_SP))
+PAT_SP_FORM = re.compile('^2a_{}.rtf$'.format(RE_SP))
+PAT_SP_FORM_PERMISSIVE = re.compile(r'.*2a_SP.*rtf$', re.IGNORECASE)
+PAT_SUBOR = re.compile(r'^[a-zA-Z0-9_.-]+$')
+
+class MessageType(Enum):
+    error = 1
+    warning = 2
+    info = 3
+
+
+class Message:
+    def __init__(self, message, path=None, type=MessageType.error):
+        self.message = message
+        self.path = path
+        self.type = type
+
+    def __str__(self):
+        ret = '[{}] '.format(self.type.name.upper())
+        if self.path:
+            ret += '{}: '.format(self.path)
+        ret += self.message
+        return ret
+
+
+class Messages:
+    def __init__(self):
+        self.messages = []
+
+    def add(self, *args, **kwargs):
+        self.messages.append(Message(*args, **kwargs))
+
+    def __str__(self):
+        return '\n'.join(str(message) for message in self.messages)
 
 
 def print_iterator(iterator):
@@ -22,6 +62,7 @@ def check_rtf(path, handler=None):
             document = parse(tokenize(bs), encoding='cp1250')
         except:
             print('{}: Chyba pri parsovani na pozicii {}\n'.format(path, bs.pos), file=sys.stderr)
+            return
 
         if handler:
             handler(path, document)
@@ -36,6 +77,83 @@ def check_formular_sp(path, document):
         print()
 
 
+def process_sp_list_dir(messages, sp_list_dir_path):
+    """Spracovava adresare s nazvom 3a_SP_ziadosti"""
+    for name in os.listdir(sp_list_dir_path):
+        path = os.path.join(sp_list_dir_path, name)
+        if not os.path.isdir(path):
+            messages.add('nie je adresar', path=path)
+            continue
+        if not PAT_SP_DIR.match(name):
+            messages.add('nevyhovuje formatu nazvu adresara pre studijny program', path=path)
+        process_sp_dir(messages, path, nazov_sp=name)
+
+
+def process_sp_dir(messages, sp_dir_path, nazov_sp=None):
+    """Spracovava adresare studijneho programu"""
+    pocet_formularov_sp = 0
+    pocet_formularov_vpch = 0
+    pocet_formularov_il = 0
+    for name in os.listdir(sp_dir_path):
+        path = os.path.join(sp_dir_path, name)
+        if PAT_SP_FORM_PERMISSIVE.match(name):
+            process_sp_form(messages, path, nazov_sp=nazov_sp)
+            pocet_formularov_sp += 1
+        else:
+            process_generic_file(messages, path)
+
+    if pocet_formularov_sp == 0:
+        messages.add('adresar neobsahuje formular SP', path=sp_dir_path)
+    else:
+        messages.add('v adresari sa nachadza viac formularov SP', path=sp_dir_path)
+
+    if pocet_formularov_il == 0:
+        messages.add('adresar neobsahuje formular IL', path=sp_dir_path)
+
+    if pocet_formularov_vpch == 0:
+        messages.add('adresar neobsahuje formular VPCH', path=sp_dir_path)
+
+
+def process_sp_form(messages, sp_form_path, nazov_sp=None):
+    name = os.path.basename(sp_form_path)
+    if not PAT_SP_FORM.match(name):
+        messages.add('nazov formulara SP nevyhovuje formatu', path=sp_form_path)
+    if nazov_sp is not None and name != '2a_{}_formular.rtf':
+        messages.add('nazov formulara SP nesuhlasi s nazvom adresara', path=sp_form_path)
+    check_rtf(sp_form_path, check_formular_sp)
+
+
+def process_generic_file(messages, path):
+    name = os.path.basename(path)
+    if not PAT_SUBOR.match(name):
+        messages.add('nazov suboru obsahuje nepovolene znaky', path=path)
+
+
+
+def guess_path_type(path):
+    path = os.path.abspath(path)
+    basename = os.path.basename(path)
+    is_dir = os.path.isdir(path)
+    is_file = os.path.isfile(path)
+    if is_dir and basename == '3a_SP_ziadosti':
+        return 'sp_list'
+    elif is_dir and PAT_SP_DIR.match(basename):
+        return 'sp'
+    elif is_file and PAT_SP_FORM_PERMISSIVE.match(basename):
+        return 'sp_form'
+    return None
+
+
+def process_path(messages, path, type):
+    if type == 'sp_list':
+        process_sp_list_dir(messages, path)
+    elif type == 'sp':
+        process_sp_dir(messages, path)
+    elif type == 'sp_form':
+        process_sp_form(messages, path)
+    else:
+        raise ValueError('Unknown path type')
+
 
 if __name__ == '__main__':
     import sys
@@ -46,16 +164,16 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('path')
+    parser.add_argument('--type', choices=('sp', 'sp_list', 'sp_form'))
     args = parser.parse_args()
 
-    if os.path.isdir(args.path):
-        for root, dirs, files in os.walk(args.path):
-            for file in files:
-                if file.endswith('.rtf'):
-                    path = os.path.join(root, file)
-                    check_rtf(path)
-    elif os.path.isfile(args.path):
-        if args.path.endswith('.rtf'):
-            check_rtf(args.path, check_formular_sp)
+    messages = Messages()
+    if args.type is None:
+        type = guess_path_type(args.path)
+        if type is None:
+            sys.stderr.write('Neviem zistit typ cesty {}\n'.format(args.path))
+            exit(1)
     else:
-        raise ValueError('Musi byt subor alebo adresar')
+        type = args.type
+    process_path(messages, args.path, type)
+    print(messages)
